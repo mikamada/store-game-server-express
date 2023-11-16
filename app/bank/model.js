93c1865dbaf0e53f8f47f0735f95bf853e55d22c
mikamada <mikamada17@gmail.com>
2023-11-16 14:07:47 +0700

    crud bank

---
+++ app/bank/model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+let bankSchema = mongoose.Schema({
+
+  name: {
+    type: String,
+    require: [true, "Nama pemilik harus diisi"]
+  },
+
+  nameBank: {
+    type: String,
+    require: [true, "Nama bank harus diisi"]
+  },
+  noRekening: {
+    type: String,
+    require: [true, "nomor rekening bank harus diisi"]
+  },
+})
+
+module.exports = mongoose.model('Bank', bankSchema)

84556de848e3afd5e751def61fc1b5860a08af49
mikamada <mikamada17@gmail.com>
2023-11-23 23:34:15 +0700

    create api history

--- app/player/router.js
+++ app/player/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage, detailPage, category, checkout } = require("./controllers");
+const { landingPage, detailPage, category, checkout, history, historyDetail } = require("./controllers");
 const { isLoginPlayer } = require("../middleware/auth")
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 
 module.exports = router
